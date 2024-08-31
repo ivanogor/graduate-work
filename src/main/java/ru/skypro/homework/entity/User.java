581f027efcfb463092a45449f3f8b6d4f61bbea4
kolia <koliamba_85@mail.ru>
2024-08-31 15:28:51 +0800

    Changed Ads controller

--- src/main/java/ru/skypro/homework/entity/User.java
+++ src/main/java/ru/skypro/homework/entity/User.java
@@ -1,6 +1,5 @@
 package ru.skypro.homework.entity;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,11 +16,18 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
+    @Column(name = "email")
     private String email;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "phone")
     private String phone;
+    @Column(name = "role")
     private Role role;
+    @Column(name = "image")
     private String image;
 }
