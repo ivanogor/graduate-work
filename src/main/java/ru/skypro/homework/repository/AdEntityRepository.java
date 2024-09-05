949ac4880c0111df1ae7cb366946fc8ac7a24f03
kolia <koliamba_85@mail.ru>
2024-09-05 12:12:57 +0800

    Rename repositories

--- src/main/java/ru/skypro/homework/repository/AdEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.skypro.homework.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.entity.AdEntity;
-
-public interface AdEntityRepository extends JpaRepository<AdEntity, Integer> {
-}
--- src/main/java/ru/skypro/homework/repository/UserEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.skypro.homework.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.entity.UserEntity;
-
-public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
-}
