05983f24c9172b5b47271dbfbcf069e69646af65
kolia <koliamba_85@mail.ru>
2024-09-09 22:42:47 +0800

    Fix image display

---
+++ src/main/java/ru/skypro/homework/controller/ImageController.java
@@ -0,0 +1,39 @@
+package ru.skypro.homework.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.utils.AdImageMapper;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
+@Tag(name = "API для работы с изображениями")
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("image")
+public class ImageController {
+    private final AdImageMapper mapper;
+
+    public ImageController(AdImageMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @GetMapping("{arg}")
+    @Operation(summary = "Получение изображения из файла")
+    public void getImage(@PathVariable String arg, HttpServletResponse response) throws IOException {
+        Path pathFile = Path.of(mapper.getPath(arg));
+        File imageFile = new File(mapper.getPath(arg));
+        try (
+                InputStream is = Files.newInputStream(pathFile);
+                OutputStream os = response.getOutputStream();
+        ) {
+            response.setStatus(200);
+            response.setContentType("image/jpeg");
+            response.setContentLength((int) imageFile.length());
+            is.transferTo(os);
+        }
+    }
+}
